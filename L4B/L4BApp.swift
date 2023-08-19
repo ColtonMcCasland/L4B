5accfda32e55a5cea0fb0a6cc15698fa3b315b98
ColtonMcCasland <36249316+ColtonMcCasland@users.noreply.github.com>
2023-08-19 08:31:16 -0500

    Initial Commit

---
+++ L4B/L4BApp.swift
@@ -0,0 +1,20 @@
+//
+//  L4BApp.swift
+//  L4B
+//
+//  Created by Colton McCasland on 8/19/23.
+//
+
+import SwiftUI
+
+@main
+struct L4BApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
