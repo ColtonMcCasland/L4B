356edca5a98a0ea4a475100fc726548e05884734
ColtonMcCasland <36249316+ColtonMcCasland@users.noreply.github.com>
2023-08-19 10:37:48 -0500

    ui progress

--- L4B/ProjectGridView.swift
+++ L4B/ProjectGridView.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 
+
 struct VisualEffectView: View {
 	var effect: NSVisualEffectView.Material
 	
@@ -32,26 +33,27 @@ struct VisualEffectViewRepresentable: NSViewRepresentable {
 }
 
 struct ProjectGridView: View {
-	@State private var items: [String] = []
-	@State private var isCreateSheetPresented = false
+	@Environment(\.managedObjectContext) private var viewContext
+	@FetchRequest(
+		sortDescriptors: [NSSortDescriptor(keyPath: \Project.timestamp, ascending: true)],
+		animation: .default)
+	private var projects: FetchedResults<Project>
+	
+	@State private var isProjectViewPresented = false
 	@State private var newProjectTitle = ""
-	@State private var newProjectType = ""
 	
 	var body: some View {
-		let columns = [
-			GridItem(.flexible()),
-			GridItem(.flexible())
-			// Add more GridItems for more columns
-		]
-		
 		ScrollView {
-			LazyVGrid(columns: columns, spacing: 20) {
-				ForEach(items, id: \.self) { item in
+			LazyVGrid(columns: [
+				GridItem(.flexible()),
+				GridItem(.flexible())
+			], spacing: 20) {
+				ForEach(projects) { project in
 					RoundedRectangle(cornerRadius: 10)
 						.frame(width: 250, height: 250)
 						.foregroundColor(Color(.darkGray))
 						.overlay(
-							Text(item)
+							Text(project.title ?? "")
 								.font(.headline)
 								.foregroundColor(.white)
 						)
@@ -59,70 +61,34 @@ struct ProjectGridView: View {
 			}
 			.padding()
 		}
+		.navigationTitle("Projects")
 		.toolbar {
-			ToolbarItem(placement: .primaryAction) {
-				HStack {
-					Button("Import") {
-						// Add your import logic here
-					}
-					.keyboardShortcut("i", modifiers: .command)
-					
-					Button("Export") {
-						// Add your export logic here
-					}
-					.keyboardShortcut("e", modifiers: .command)
-					
-					Button("Create") {
-						isCreateSheetPresented.toggle()
-					}
-					.keyboardShortcut("n", modifiers: .command)
+			ToolbarItem {
+				Button(action: {
+					isProjectViewPresented.toggle()
+				}) {
+					Label("Create Project", systemImage: "plus")
 				}
 			}
 		}
-		.sheet(isPresented: $isCreateSheetPresented, content: {
-			CreateProjectSheet(
-				isPresented: $isCreateSheetPresented,
-				newProjectTitle: $newProjectTitle,
-				newProjectType: $newProjectType,
-				onSave: createNewProject
-			)
+		.sheet(isPresented: $isProjectViewPresented, content: {
+			ProjectView(onSave: addProject)
 		})
 	}
 	
-	func createNewProject() {
-		let timestamp = "\(Date().timeIntervalSince1970)"
-		let newProject = "\(newProjectTitle) - \(newProjectType)"
-		items.append(newProject)
-		newProjectTitle = ""
-		newProjectType = ""
+	private func addProject(title: String) {
+		withAnimation {
+			let newProject = Project(context: viewContext)
+			newProject.timestamp = Date()
+			newProject.title = title
+			
+			do {
+				try viewContext.save()
+			} catch {
+				let nsError = error as NSError
+				fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+			}
+		}
 	}
 }
 
-struct CreateProjectSheet: View {
-	@Binding var isPresented: Bool
-	@Binding var newProjectTitle: String
-	@Binding var newProjectType: String
-	var onSave: () -> Void
-	
-	var body: some View {
-		VStack {
-			Text("Create New Project")
-				.font(.headline)
-				.padding(.bottom, 10)
-			
-			TextField("Project Title", text: $newProjectTitle)
-				.textFieldStyle(RoundedBorderTextFieldStyle())
-				.padding(.bottom, 10)
-			
-			TextField("Project Type", text: $newProjectType)
-				.textFieldStyle(RoundedBorderTextFieldStyle())
-				.padding(.bottom, 20)
-			
-			Button("Save") {
-				onSave()
-				isPresented.toggle()
-			}
-		}
-		.padding()
-	}
-}
