58b6c6e134dbbe7d1937ad6c6680f2c05cbd2af0
ColtonMcCasland <36249316+ColtonMcCasland@users.noreply.github.com>
2023-08-20 09:20:58 -0500

    .

--- L4B/CubeController.swift
+++ L4B/CubeController.swift
@@ -17,16 +17,50 @@ struct CubeControllerView: NSViewRepresentable {
 		sceneView.allowsCameraControl = false
 		
 		let cube = SCNBox(width: 2, height: 2, length: 2, chamferRadius: 0)
-		cube.firstMaterial?.diffuse.contents = NSColor.white
-		cube.firstMaterial?.specular.contents = NSColor.gray
+		
+		// Create a white material for the faces
+		let faceMaterial = SCNMaterial()
+		faceMaterial.diffuse.contents = NSColor.white
+		
+		// Create a gray material for the edges
+		let edgeMaterial = SCNMaterial()
+		edgeMaterial.diffuse.contents = NSColor.gray
+		
+		// Set the materials for each face
+		cube.materials = [faceMaterial, faceMaterial, faceMaterial, faceMaterial, faceMaterial, faceMaterial]
 		
 		let cubeNode = SCNNode(geometry: cube)
 		sceneView.scene?.rootNode.addChildNode(cubeNode)
 		
+		// Create the vertices for the edges
+		let vertices: [SCNVector3] = [
+			// Vertices for each corner
+			SCNVector3(-1, 1, 1), SCNVector3(1, 1, 1),
+			SCNVector3(-1, -1, 1), SCNVector3(1, -1, 1),
+			SCNVector3(-1, 1, -1), SCNVector3(1, 1, -1),
+			SCNVector3(-1, -1, -1), SCNVector3(1, -1, -1),
+		]
+		
+		// Define indices for all the edges
+		let edgeIndices: [UInt32] = [
+			0, 1, 1, 3, 3, 2, 2, 0,  // Top edges
+			4, 5, 5, 7, 7, 6, 6, 4,  // Bottom edges
+			0, 4, 1, 5, 2, 6, 3, 7,  // Vertical edges
+		]
+		
+		let edgeVertexSource = SCNGeometrySource(vertices: vertices)
+		let edgeIndexElement = SCNGeometryElement(indices: edgeIndices, primitiveType: .line)
+		
+		// Create the edge geometry
+		let edgeGeometry = SCNGeometry(sources: [edgeVertexSource], elements: [edgeIndexElement])
+		edgeGeometry.firstMaterial = edgeMaterial
+		
+		let edgeNode = SCNNode(geometry: edgeGeometry)
+		cubeNode.addChildNode(edgeNode)
+		
 		addText("Top", to: cubeNode, at: SCNVector3(0, 1, 0), rotation: SCNVector4(1, 0, 0, -CGFloat.pi / 2))
 		addText("Bottom", to: cubeNode, at: SCNVector3(0, -1, 0), rotation: SCNVector4(1, 0, 0, CGFloat.pi / 2))
-		//		addText("Left", to: cubeNode, at: SCNVector3(-1, 0, 0), rotation: SCNVector4(0, 1, 0, CGFloat.pi / 2))
-		//		addText("Right", to: cubeNode, at: SCNVector3(1, 0, 0), rotation: SCNVector4(0, 1, 0, -CGFloat.pi / 2))
+		// Add text for left and right here
 		
 		// Position the camera to view the cube
 		let cameraNode = SCNNode()
@@ -40,6 +74,10 @@ struct CubeControllerView: NSViewRepresentable {
 		
 		return sceneView
 	}
+
+
+
+
 	
 	func addText(_ text: String, to node: SCNNode, at position: SCNVector3, rotation: SCNVector4) {
 		let textGeometry = SCNText(string: text, extrusionDepth: 0.1)
