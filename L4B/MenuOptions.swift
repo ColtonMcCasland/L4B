2d907b5309639dc923a73151d538530210cb5d56
ColtonMcCasland <36249316+ColtonMcCasland@users.noreply.github.com>
2023-08-27 11:32:32 -0500

    .

--- L4B/MenuOptions.swift
+++ L4B/MenuOptions.swift
@@ -6,49 +6,20 @@ struct MenuOptions: View {
 		HStack(spacing: 0) {
 			
 			Spacer()
-			
-			// Logic to draw shapes
-			
+						
 			HStack {
+				
 				VStack {
-					Spacer()
 					
+
 					Button("Line") {
 						// Handle drawing shapes
 					}
-					Button("Poly Line") {
-						// Handle drawing shapes
-					}
 					Spacer()
 				}
 
-				
-				VStack {
-					Spacer()
-					Button("Arc") {
-						// Handle drawing shapes
-					}
-					Button("Circle") {
-						// Handle drawing shapes
-					}
-					Spacer()
-				}
+				Spacer()
 			}
-			
-
-			
-			// Logic to create 3d mesh objects and modify them.
-
-			//			TBD...
-//			Button("Create 3D Objects") {
-//				// Handle 3D objects
-//			}
-//			Spacer()
-//			//			TBD...
-//			Button("Add Chamfers") {
-//				// Handle chamfers
-//			}
-			Spacer()
 		}
 	}
 }
--- L4B/SandboxView.swift
+++ L4B/SandboxView.swift
@@ -1,24 +1,32 @@
 import SwiftUI
 import SceneKit
 
-
 struct SandboxView: View {
 	@ObservedObject var rotationState = RotationState()
-	@ObservedObject var positionState = PositionState() // Add this line
+	@ObservedObject var positionState = PositionState()
 	
-
 	var body: some View {
 		ZStack {
-			SandboxContentView(rotationState: rotationState) // Pass both states here
+			SandboxContentView(rotationState: rotationState)
+			
 			VStack {
+				ZStack {
+					FrostedGlassMenu()
+						.frame(height: 80)  // Only set the height
+
+					MenuOptions()
+						.frame(height: 40)  // Match the height
+				}
 				
+				// Cube below the header
 				HStack {
 					Spacer()
 					CubeControllerView(rotationState: rotationState)
-					.frame(width: 100, height: 100)
+						.frame(width: 100, height: 100)
 						.padding(.top, 10)
 						.padding(.trailing, 10)
 				}
+				
 				Spacer()
 			}
 		}
