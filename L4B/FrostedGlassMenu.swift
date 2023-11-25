b898043343c98918dd931e58098828c5ef5a5c09
ColtonMcCasland <36249316+ColtonMcCasland@users.noreply.github.com>
2023-11-24 19:00:01 -0600

    frosted glass change

--- L4B/FrostedGlassMenu.swift
+++ L4B/FrostedGlassMenu.swift
@@ -7,9 +7,9 @@ struct FrostedGlassMenu: NSViewRepresentable {
 	func makeNSView(context: Context) -> NSVisualEffectView {
 		let view = NSVisualEffectView()
 		
-		// Use the .fullScreenUI appearance to emulate an iOS frosted glass style
-		view.material = .fullScreenUI
-		view.blendingMode = .behindWindow
+		// Adjust the material and blending mode for a lighter, more translucent effect
+		view.material = .hudWindow // or .menu for a slightly different effect
+		view.blendingMode = .withinWindow // Adjust the blending mode for a lighter effect
 		view.state = .active
 		
 		// Apply corner radius only to the top corners
@@ -17,9 +17,11 @@ struct FrostedGlassMenu: NSViewRepresentable {
 		view.layer?.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
 		view.layer?.cornerRadius = 20
 		
+		// Set the background color to clear for added transparency
+		view.layer?.backgroundColor = NSColor.clear.cgColor
+		
 		return view
 	}
-	
 	
 	func updateNSView(_ nsView: NSVisualEffectView, context: Context) {
 		// Update view if needed
--- L4B/MenuOptions.swift
+++ L4B/MenuOptions.swift
@@ -3,7 +3,7 @@ import SceneKit
 
 struct MenuOptions: View {
 	var body: some View {
-		HStack(spacing: 0) {
+		HStack() {
 			
 			Spacer()
 						
