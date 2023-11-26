37cf6b71eb44652dc4ac5f15ee705ea367534694
ColtonMcCasland <36249316+ColtonMcCasland@users.noreply.github.com>
2023-11-26 12:01:17 -0600

    cleaning

--- L4B/MenuOptions.swift
+++ L4B/MenuOptions.swift
@@ -4,18 +4,7 @@ import SceneKit
 struct MenuOptions: View {
 	var body: some View {
 		VStack {
-			// Add more Spacer elements to move the arrow down
-			Spacer()
-			Spacer()
 			
-			HStack {
-				Spacer()
-				
-				// Add the chevron arrow pointing down
-				Image(systemName: "chevron.down")
-				
-				Spacer()
-			}
 		}
 	}
 }
